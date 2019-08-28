14c2ae9db85c5b8e1c4643413ec9e9b709e5c80c
dsstewa <davidstewart@gmail.com>
2019-08-28 17:38:30 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,29 @@
+require 'pry'
+
 class SessionsController < ApplicationController
   
   def new
+    
   end
  
   def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/sessions/new'
+    else
+      
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+
 
   end
    
-  def destory
-
+  def destroy
+    
+    
+      session.delete :name
+      redirect_to '/sessions/new'
+     
   end
 
 
